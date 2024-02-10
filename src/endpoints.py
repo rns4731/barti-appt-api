fec4251cf4931233025ebe1bd3628dcc6ae0fa83
Rohit <nrohits@gmail.com>
2024-02-10 00:40:47 -0800

    first appointment working

--- src/endpoints.py
+++ src/endpoints.py
@@ -4,6 +4,7 @@ from src.extensions import db
 from src.models import DummyModel, Doctor, WorkingHours, Appointment
 from webargs import fields
 from webargs.flaskparser import use_args
+from datetime import datetime
 
 home = Blueprint('/', __name__)
 
@@ -29,7 +30,6 @@ def dummy_model(id_):
 @home.route('/dummy_model', methods=['POST'])
 @use_args({'value': fields.String()})
 def dummy_model_create(args):
-    print(args)
     new_record = DummyModel(value=args.get('value'))
     db.session.add(new_record)
     db.session.commit()
@@ -55,4 +55,71 @@ def doctors():
 @home.route('/appointments', methods=['GET'])
 def appointments():
     records = Appointment.query.all()
+    return jsonify([record.json().json for record in records])
+
+
+@home.route('/appointments', methods=['POST'])
+@use_args({
+    'doctor_id': fields.Integer(required=True),
+    'duration': fields.Integer(required=True),
+    'start_time': fields.String(required=True),
+    'end_time': fields.String(required=True)
+}, location='json')
+def appointments_create(args):
+    format = "%Y-%m-%d %H:%M"
+    start_time = args.get('start_time')
+    end_time = args.get('end_time')
+    for t in [start_time, end_time]:
+        try:
+            datetime.strptime(t, format)
+        except ValueError:
+            return jsonify({'error': f"Invalid date format for {t}. Please use {format}"}), HTTPStatus.BAD_REQUEST
+    
+    doctor = Doctor.query.filter_by(id=args.get('doctor_id')).first()
+    if not doctor.has_availability(start_time, end_time):
+        return jsonify({'error': f"Doctor {doctor.name} is not available at the given time window"}), HTTPStatus.BAD_REQUEST
+    if doctor.has_appointment_overlap(start_time, end_time):
+        return jsonify({'error': f"Doctor {doctor.name} has an appointment at the given time window"}), HTTPStatus.BAD_REQUEST
+    new_record = Appointment(doctor_id=args.get('doctor_id'), duration=args.get('duration'), start_time=start_time, end_time=end_time)
+    db.session.add(new_record)
+    db.session.commit()
+
+
+@home.route("/appointments/<id_>/first_available", methods=['POST'])
+@use_args({
+    'start_time': fields.String(required=True),
+    "duration": fields.Integer(required=True)
+}, location='query')
+def first_available_appointment(args, id_):
+    format = "%Y-%m-%d %H:%M"
+    start_time = args.get('start_time')
+    duration = args.get('duration')
+    for t in [start_time]:
+        try:
+            datetime.strptime(t, format)
+        except ValueError:
+            return jsonify({'error': f"Invalid date format for {t}. Please use {format}"}), HTTPStatus.BAD_REQUEST
+    doctor = Doctor.query.filter_by(id=id_).first()
+    appointment = doctor.first_appointment(start_time, duration)
+    if appointment is not None:
+        return appointment.json()
+    else:
+        return jsonify(None), HTTPStatus.NOT_FOUND
+
+
+@home.route('/appointments/doctor/<id_>', methods=['GET'])
+@use_args({
+    'start_time': fields.String(required=True),
+    "end_time": fields.String(required=True)
+}, location='query')
+def appointments_by_doctor_window(args, id_):
+    format = "%Y-%m-%d %H:%M"
+    start_time = args.get('start_time')
+    end_time = args.get('end_time')
+    for t in [start_time, end_time]:
+        try:
+            datetime.strptime(t, format)
+        except ValueError:
+            return jsonify({'error': f"Invalid date format for {t}. Please use {format}"}), HTTPStatus.BAD_REQUEST
+    records = Appointment.query.filter_by(doctor_id=id_).filter(Appointment.start_time >= start_time).filter(Appointment.end_time <= end_time).all()
     return jsonify([record.json().json for record in records])
--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
+from datetime import timedelta
 from src.extensions import db
 from flask import jsonify
-
+from datetime import datetime
 
 class DummyModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -18,6 +19,65 @@ class Doctor(db.Model):
     email = db.Column(db.String, nullable=False)
     working_hours = db.relationship('WorkingHours', backref='doctor', lazy=True)
     appointments = db.relationship('Appointment', backref='doctor', lazy=True)
+
+    def has_availability(self, start_time, end_time) -> bool:
+        """
+        Checks if the doctor has availability for the given time window
+        """
+        for working_hour in self.working_hours:
+            if working_hour.day_of_week == start_time.weekday() and working_hour.start_time >= start_time.time() and working_hour.end_time <= end_time.time():
+                return True
+        return False
+    
+    def has_appointment_overlap(self, start_time, end_time) -> bool:
+        """
+        Checks if the doctor has an appointment at the given time window
+        """
+        for appointment in self.appointments:
+            if appointment.start_time >= start_time or appointment.end_time <= end_time:
+                return True
+        return False
+    
+    def first_appointment(self, start_time, duration) -> str:
+        """
+        Returns the first available appointment for the given time window
+        """
+        sorted_working_hours = self.working_hours.order_by(WorkingHours.day_of_week.asc())
+        current_day = start_time.weekday()
+        end_time = start_time + timedelta(minutes=duration)
+        filtered_working_hours = list(filter(lambda x: x.day_of_week >= current_day, sorted_working_hours))
+
+        for working_hour in filtered_working_hours:
+            if working_hour.start_time <= start_time.time() and working_hour.end_time >= end_time.time():
+                # We have found a working hour that fits the time window
+                # Now we need to check if there are any appointments that overlap with this time window
+                # If same day, then use current start and end times
+                if working_hour.start_time.weekday() == current_day:
+                    target_start_time = start_time
+                    target_end_time = end_time
+                else:
+                    # If not same day, then use the start time of the working hour
+                    target_start_time = working_hour.start_time
+                    target_end_time = target_start_time + timedelta(minutes=duration)
+
+                # Get appointments for the same day
+                appointments = self.appointments.filter(Appointment.start_time.weekday() == target_start_time.weekday()).order_by(Appointment.start_time.asc())
+                for appointment in appointments:
+                    if (
+                        (target_start_time >= appointment.start_time and target_end_time <= appointment.end_time) # In between
+                        or (target_start_time <= appointment.start_time and target_end_time >= appointment.start_time) # Overlapping start
+                        or (target_start_time <= appointment.end_time and target_end_time >= appointment.end_time) # Overlapping end
+                    ):
+                        # There is an appointment that overlaps with the target time window, move time window
+                        target_start_time = appointment.end_time
+                        target_end_time = target_start_time + timedelta(minutes=duration)
+                
+                if target_end_time <= working_hour.end_time:
+                    # Check within working hours
+                    appointment = Appointment(doctor_id=self.id, duration=duration, start_time=target_start_time, end_time=target_end_time)
+                    return appointment
+        return None
+
 
     def json(self) -> str:
         """
